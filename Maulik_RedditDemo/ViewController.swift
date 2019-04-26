64efe9b94b9846fc44d1ff7f5ba0b7ae41fa6895
Maulik Bhuptani <bhuptanimaulikp@gmail.com>
2019-04-26 21:26:57 +0530

    Initial Commit

---
+++ Maulik_RedditDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Maulik_RedditDemo
+//
+//  Created by Maulik Bhuptani on 26/04/19.
+//  Copyright © 2019 Maulik Bhuptani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Maulik_RedditDemoTests/Maulik_RedditDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  Maulik_RedditDemoTests.swift
+//  Maulik_RedditDemoTests
+//
+//  Created by Maulik Bhuptani on 26/04/19.
+//  Copyright © 2019 Maulik Bhuptani. All rights reserved.
+//
+
+import XCTest
+@testable import Maulik_RedditDemo
+
+class Maulik_RedditDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
